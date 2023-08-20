9d27f1fb4d33b632fc88b42befbde07676ae6d9d
TonyDolenti <antoniobdolenti@gmail.com>
2023-08-20 18:40:45 -0400

    Add project files.

---
+++ SupercarPulse/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SupercarPulse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
